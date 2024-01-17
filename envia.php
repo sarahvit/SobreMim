383f38d194969e9dc4e63345b38b90306b530559
sarahvit <sarahmalenaaquino54@gmail.com>
2024-01-17 08:59:14 -0300

    commit

--- envia.php
+++ envia.php
@@ -6,7 +6,17 @@
     $mensagem = addslashes($_POST['mensagem']);
 
     $para = "sasavit0700@gmail.com";
-    $assunto = "Coleta de dados - Portifólio"
+    $assunto = "Coleta de dados - Portifólio";
+
+    $corpo = "Nome: ".$nome."\n"."E-mail ".$email."\n"."Telefone: ".$telefone."\n"."Mensagem: ".$mensagem;
+
+    $cabeca = "From sarahmalenaaquino54@gmail.com"."\n"."Reply-to: ".$email."\n"."X=Mailer:PHP/".phpversion();
+
+    if(mail($para,$assunto,$copro,$cabeca)){
+        echo("E-mail enviado com sucesso!");
+    }else{
+        echo("Houve um erro ao enviar o email!");
+    }
 
 
 
