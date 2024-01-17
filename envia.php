1d49e15271516b5c75c4b05590bced5d7faa32e3
sarahvit <sarahmalenaaquino54@gmail.com>
2024-01-17 08:44:28 -0300

    commit

---
+++ envia.php
@@ -0,0 +1,13 @@
+<?php
+
+    $nome = addslashes($_POST['nome']);
+    $email = addslashes($_POST['email']);
+    $telefone = addslashes($_POST['telefone']);
+    $mensagem = addslashes($_POST['mensagem']);
+
+    $para = "sasavit0700@gmail.com";
+    $assunto = "Coleta de dados - Portifólio"
+
+
+
+?>
